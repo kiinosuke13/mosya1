7bde1621accf1e098d19e0223b3bdbefe7917e8b
Kisara Nishikawa <nishikawa@overwrite.work>
2023-05-17 07:15:44 +0900

    [https://code-step.com/demo/html/profile/]サイトを模写コーティング

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const hamburger = document.getElementById('hamburger');
+const span = document.querySelector('span');
+const mask = document.getElementById('mask');
+const modal = document.getElementById('modal');
+
+mask.classList.add('hidden');
+modal.classList.add('hidden');
+
+hamburger.addEventListener('click',()=>{
+    mask.classList.remove('hidden');
+    modal.classList.remove('hidden');
+    hamburger.classList.add('active');
+});
